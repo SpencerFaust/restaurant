7092407338da19d3929a9686b355e851aaeb9bd7
Spencer Faust <faus0059@umn.edu>
2019-02-13 15:40:58 -0600

    Added routes.

---
+++ server/routes/restaurant.router.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const router = express.Router();
+const pool = require('../modules/pool');
+
+router.get('/restaurant', (req, res) => {
+    console.log('/restaurant GET received');
+    pool.query(`SELECT * FROM "restaurants"`).then((response) => {
+        console.log('Response from DB:', response);
+        res.send(response.rows);
+    }).catch((error) => {
+        console.log('/restaurant GET DB error:', error);
+    });
+});
+
+router.post('/restaurant', (req, res) => {
+    console.log('/restaurant POST received');
+    pool.query(`INSERT INTO "restaurants" (name, type) VALUES ($1, $2);`, [req.body.name, req.body.type])
+    .then(() => {
+        res.sendStatus(201);
+    }).catch((error) => {
+        console.log('/restaurant POST error:', error);
+        res.sendStatus(500);
+    });
+});
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -3,35 +3,12 @@ let app = express();
 const bodyParser = require('body-parser');
 const PORT = 5000;
 const pool = require('./modules/pool');
+const restaurantRouter = require('./routes/restaurant.router');
 
 
 app.use(express.static('server/public'));
 app.use(bodyParser.urlencoded({extended: true}));
-
-app.get('/restaurant', (req, res) => {
-    console.log('/restaurant GET received');
-    pool.query(`SELECT * FROM "restaurants"`).then((response) => {
-        console.log('Response from DB:', response);
-        res.send(response.rows);
-    }).catch((error) => {
-        console.log('/restaurant GET DB error:', error);
-    });
-});
-
-app.post('/restaurant', (req, res) => {
-    console.log('/restaurant POST received');
-    pool.query(`INSERT INTO "restaurants" (name, type) VALUES ($1, $2);`, [req.body.name, req.body.type])
-    .then(() => {
-        res.sendStatus(201);
-    }).catch((error) => {
-        console.log('/restaurant POST error:', error);
-        res.sendStatus(500);
-    });
-});
-
-app.delete('/restaurant', (req, res) => {
-    pool.query(`DELETE FROM "restaurant" WHERE "name" = $1;`, [req.body.name])
-})
+app.use('/', restaurantRouter);
 
 app.listen(PORT, () => {
     console.log('Listening on port', PORT);
